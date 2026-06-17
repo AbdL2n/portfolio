542b8dc4d21c4e3ac0f9796c2bdc40bd7e6047bd
AbdL2n <kader@Student-laptop.localdomain>
2026-06-17 10:01:44 +0200

    Ajout nom de domaine

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abdl2n.github.io',
-  base: '/portfolio',
+  site: 'https://ouaali.fr',
+  // base: '/portfolio', Mode prod
   vite: {
     plugins: [tailwindcss()]
   },
